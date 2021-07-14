8818ba91c0c1812064ccf0f009fc50a49803722e
AbdulMoiz8994 <moiza8994@gmail.com>
2021-07-15 00:23:25 +0500

    add the practice of Ts

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 var names = "Abdul moiz";
+var value = 12;
+var isTure = true;
+names = "Abdul Rafay";
+console.log(names);
+var arr = ["Abdul moiz", "Abdul Rafay", "Abdul Malik", "Rizwan"];
+// we cna not assign the number and any type here
+arr.push("Abdu basit");
+arr[0] = "Whadan";
+console.log(arr);
+var arr1 = ["Abdul moiz", 1, true];
+var arr2 = [];
+arr2 = ["Abdul moiz", "Khanzada"];
+var nameFirst = "Abdulllah";
+var NumberFirst;
+NumberFirst = 1;
+var arr3;
+// arr3=true
+// arr3=1
+var obj = {
+    Name: " Abduo moiz",
+    fatherNmae: "zafar ali",
+    age: 20
+};
+console.log(obj.age);
+var obj1;
+// obj1.firstName="Names";
+// obj1.isAge=12
+obj1 = {
+    firstName: "moiz",
+    lastName: 10,
+    isAge: 24,
+    isTure: false
+};
+// console.log(obj1);
--- index.ts
+++ index.ts
@@ -1,2 +1,55 @@
-const names = "Abdul moiz";
+let names = "Abdul moiz";
+let value=12;
+let isTure=true;
+
+names="Abdul Rafay"
+console.log(names);
+let arr=["Abdul moiz", "Abdul Rafay", "Abdul Malik" ,"Rizwan"]
+// we cna not assign the number and any type here
+
+arr.push("Abdu basit")
+arr[0]="Whadan"
+console.log(arr);
+let arr1=["Abdul moiz", 1, true];
+
+
+let arr2: string[]=[];
+arr2=["Abdul moiz", "Khanzada"]
+let nameFirst:string="Abdulllah"
+let NumberFirst:number;
+NumberFirst=1
+
+let arr3: string | boolean;
+// arr3=true
+// arr3=1
+let obj={
+    Name:" Abduo moiz",
+    fatherNmae: "zafar ali",
+    age: 20
+}
+
+
+console.log(obj.age);
+
+let obj1:{
+    firstName: string,
+     lastName: number,
+     isAge: number,
+     isTure: boolean
+}
+
+
+// obj1.firstName="Names";
+// obj1.isAge=12
+
+obj1={
+  firstName: "moiz",
+  lastName: 10,
+  isAge: 24,
+  isTure: false
+
+}
+// console.log(obj1);
+
+
 
